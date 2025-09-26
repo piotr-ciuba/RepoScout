1f48a946962cadacf8e586b69fe6560f4eccbf3d
piotr ciuba <ciuba.piotr@gmail.com>
2025-09-26 11:20:28 +0200

    Color pallete generated

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Green
+  static const Color primaryGreen100 = Color(0xFFE8F5E9);
+  static const Color primaryGreen200 = Color(0xFFC8E6C9);
+  static const Color primaryGreen300 = Color(0xFFA5D6A7);
+  static const Color primaryGreen400 = Color(0xFF81C784);
+  static const Color primaryGreen500 = Color(0xFF66BB6A);
+  static const Color primaryGreen600 = Color(0xFF4CAF50);
+  static const Color primaryGreen700 = Color(0xFF43A047);
+  static const Color primaryGreen800 = Color(0xFF388E3C);
+  static const Color primaryGreen900 = Color(0xFF2E7D32);
+
+  // Secondary Green
+  static const Color secondaryGreen100 = Color(0xFFE0F2F1);
+  static const Color secondaryGreen200 = Color(0xFFB2DFDB);
+  static const Color secondaryGreen300 = Color(0xFF80CBC4);
+  static const Color secondaryGreen400 = Color(0xFF4DB6AC);
+  static const Color secondaryGreen500 = Color(0xFF26A69A);
+  static const Color secondaryGreen600 = Color(0xFF009688);
+  static const Color secondaryGreen700 = Color(0xFF00897B);
+  static const Color secondaryGreen800 = Color(0xFF00796B);
+  static const Color secondaryGreen900 = Color(0xFF004D40);
+
+  // Primary Red
+  static const Color primaryRed100 = Color(0xFFFFEBEE);
+  static const Color primaryRed200 = Color(0xFFFFCDD2);
+  static const Color primaryRed300 = Color(0xFFEF9A9A);
+  static const Color primaryRed400 = Color(0xFFE57373);
+  static const Color primaryRed500 = Color(0xFFEF5350);
+  static const Color primaryRed600 = Color(0xFFF44336);
+  static const Color primaryRed700 = Color(0xFFE53935);
+  static const Color primaryRed800 = Color(0xFFD32F2F);
+  static const Color primaryRed900 = Color(0xFFB71C1C);
+
+  // Primary Blue
+  static const Color primaryBlue100 = Color(0xFFE3F2FD);
+  static const Color primaryBlue200 = Color(0xFFBBDEFB);
+  static const Color primaryBlue300 = Color(0xFF90CAF9);
+  static const Color primaryBlue400 = Color(0xFF64B5F6);
+  static const Color primaryBlue500 = Color(0xFF42A5F5);
+  static const Color primaryBlue600 = Color(0xFF2196F3);
+  static const Color primaryBlue700 = Color(0xFF1E88E5);
+  static const Color primaryBlue800 = Color(0xFF1976D2);
+  static const Color primaryBlue900 = Color(0xFF0D47A1);
+
+  // Primary Orange
+  static const Color primaryOrange100 = Color(0xFFFFF3E0);
+  static const Color primaryOrange200 = Color(0xFFFFE0B2);
+  static const Color primaryOrange300 = Color(0xFFFFCC80);
+  static const Color primaryOrange400 = Color(0xFFFFB74D);
+  static const Color primaryOrange500 = Color(0xFFFFA726);
+  static const Color primaryOrange600 = Color(0xFFFF9800);
+  static const Color primaryOrange700 = Color(0xFFFB8C00);
+  static const Color primaryOrange800 = Color(0xFFF57C00);
+  static const Color primaryOrange900 = Color(0xFFEF6C00);
+
+  // Primary White
+  static const Color primaryWhite100 = Color(0xFFFFFFFF);
+  static const Color primaryWhite200 = Color(0xFFFFFFFF);
+  static const Color primaryWhite300 = Color(0xFFFFFFFF);
+  static const Color primaryWhite400 = Color(0xFFFFFFFF);
+  static const Color primaryWhite500 = Color(0xFFFFFFFF);
+  static const Color primaryWhite600 = Color(0xFFFFFFFF);
+  static const Color primaryWhite700 = Color(0xFFFFFFFF);
+  static const Color primaryWhite800 = Color(0xFFFFFFFF);
+  static const Color primaryWhite900 = Color(0xFFFFFFFF);
+
+  // Primary Grey
+  static const Color primaryGrey100 = Color(0xFFFAFAFA);
+  static const Color primaryGrey200 = Color(0xFFF5F5F5);
+  static const Color primaryGrey300 = Color(0xFFEEEEEE);
+  static const Color primaryGrey400 = Color(0xFFE0E0E0);
+  static const Color primaryGrey500 = Color(0xFFBDBDBD);
+  static const Color primaryGrey600 = Color(0xFF9E9E9E);
+  static const Color primaryGrey700 = Color(0xFF757575);
+  static const Color primaryGrey800 = Color(0xFF616161);
+  static const Color primaryGrey900 = Color(0xFF424242);
+}
